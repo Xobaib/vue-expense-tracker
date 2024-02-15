1858e10249a84488e2b06b3aed5adadec89b160d
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-02-15 11:09:31 +0330

    fetch from local storage

--- src/App.vue
+++ src/App.vue
@@ -19,16 +19,19 @@ import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 import { useToast } from "vue-toastification";
 
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 
 const toast = useToast();
 
-const transactions = ref([
-  { id: 1, text: "Flower", amount: -19.99 },
-  { id: 2, text: "Salary", amount: 299.97 },
-  { id: 3, text: "Book", amount: -10 },
-  { id: 4, text: "Camera", amount: 150 },
-]);
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTaransactions = JSON.parse(localStorage.getItem("transactions"));
+
+  if (savedTaransactions) {
+    transactions.value = savedTaransactions;
+  }
+});
 
 // get total
 const total = computed(() => {
